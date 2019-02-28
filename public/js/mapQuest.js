541e7de049f58e80b6f71a055ba34eadf37b5168
jen <jes089@ucsd.edu>
2019-02-28 11:47:49 -0800

    all of it

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,14 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'SvXoFzF9DRiAeA8sKJ74Rw7TcFtiutM9';
+
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false 
+	});
+
+	// Add a marker to the map 
+	L.marker([32.88, -117.236]).addTo(map);
 }
